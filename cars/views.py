842f663592d232d5dda98be73d3638892e3f5d4b
Thami <thamibetin@gmail.com>
2024-06-01 22:47:26 -0300

    adiciona formularios para cadastro de carros com ModelForm do django

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
+from cars.forms import CarModelForm
 
 def cars_view(request):
     cars = Car.objects.all().order_by('model')
@@ -11,4 +12,15 @@ def cars_view(request):
     return render(request,
                   'cars.html',
                   {'cars': cars}
-    )   
+    )
+
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list') # envia o form pra view e envia o client p/ a lista de carros
+    else:
+        new_car_form = CarModelForm() # cria o formulário vazio
+    return render(request, 'new_car.html', {'new_car_form': new_car_form})
